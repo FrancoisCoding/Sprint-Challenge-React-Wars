ab42ddcf49091c2060e302a604712904497964d8
Isaiah Francois <francoisisaiaht@yahoo.com>
2019-07-19 11:55:24 -0400

    Mapped over data and returned to page

--- starwars/src/components/People.js
+++ starwars/src/components/People.js
@@ -1,8 +1,18 @@
 import React, { Component } from "react";
+import Card from "./Card";
 
 export default class Objects extends Component {
   render() {
-    console.log(this.props);
-    return <div className="card" />;
+    if (!this.props.data.results) {
+      return <h1 className="hide">Loading...</h1>;
+    }
+
+    return (
+      <div>
+        {this.props.data.results.map(person => (
+          <Card data={person} />
+        ))}
+      </div>
+    );
   }
 }
