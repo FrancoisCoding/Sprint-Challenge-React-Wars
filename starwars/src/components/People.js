8ba46f949b0ca42cec368d0b0b8812574e0b2810
Isaiah Francois <francoisisaiaht@yahoo.com>
2019-07-20 12:57:49 -0400

    refactored people component to functional based component

--- starwars/src/components/People.js
+++ starwars/src/components/People.js
@@ -1,18 +1,17 @@
 import React, { Component } from "react";
 import Cards from "./Cards";
 
-export default class Objects extends Component {
-  render() {
-    if (!this.props.data.results) {
-      return <h1 className="hide">Loading...</h1>;
-    }
-
-    return (
-      <div>
-        {this.props.data.results.map(person => (
-          <Cards key={person.name} data={person} />
-        ))}
-      </div>
-    );
+function Objects(props) {
+  if (!props.data.results) {
+    return <h1 className="hide">Loading...</h1>;
   }
+
+  return (
+    <div>
+      {props.data.results.map(person => (
+        <Cards key={person.name} data={person} />
+      ))}
+    </div>
+  );
 }
+export default Objects;
