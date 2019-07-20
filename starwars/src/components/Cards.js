592266a3e41b29bbe926960d55939ac8b1ac1378
Isaiah Francois <francoisisaiaht@yahoo.com>
2019-07-20 12:59:17 -0400

    refactored cards component to functional based component

--- starwars/src/components/Cards.js
+++ starwars/src/components/Cards.js
@@ -1,25 +1,24 @@
 import React, { Component } from "react";
 
-export default class Cards extends Component {
-  render() {
-    console.log(this.props.data);
-    return (
-      <div className="card center">
-        <img
-          className="card-img-top"
-          src="https://cdn.pixabay.com/photo/2016/12/13/04/22/toys-1903273__480.jpg"
-          alt="Star Wars"
-        />
-        <div className="card-body">
-          <h1 className="card-title">Name: {this.props.data.name}</h1>
-          <div className="card-text">
-            <h5 className="mb-0 mt-3">
-              <span className="mr-1 ml-2 font-weight-bold">BirthYear :</span>
-              <span className="ml-1">{this.props.data.birth_year}</span>
-            </h5>
-          </div>
+function Cards(props) {
+  return (
+    <div className="card center">
+      <img
+        className="card-img-top"
+        src="https://cdn.pixabay.com/photo/2016/12/13/04/22/toys-1903273__480.jpg"
+        alt="Star Wars"
+      />
+      <div className="card-body">
+        <h1 className="card-title">Name: {props.data.name}</h1>
+        <div className="card-text">
+          <h5 className="mb-0 mt-3">
+            <span className="mr-1 ml-2 font-weight-bold">BirthYear :</span>
+            <span className="ml-1">{props.data.birth_year}</span>
+          </h5>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
+
+export default Cards;
